fadfec25a4827338a443d0d6e7ba81b239aff29d
AlineEspindola <aline.ae@aluno.ifsc.edu.br>
2025-10-24 19:24:18 -0300

    chore(frontend-1): Faz config básica par rodar os testes

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,10 +25,11 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    headless: false,
     video: 'on',
     screenshot: 'only-on-failure',
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: process.env.BASE_URL || "https://www.saucedemo.com/",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
