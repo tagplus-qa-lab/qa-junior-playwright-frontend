6f734fcc3b10615e66bad0a269e33fd502ecc335
AlineEspindola <aline.ae@aluno.ifsc.edu.br>
2025-10-24 22:39:13 -0300

    refactor(frontend-5): Coloca os seletores em um mesmo local

--- src/pages/LoginPage.ts
+++ src/pages/LoginPage.ts
@@ -1,6 +1,6 @@
 import { Locator, Page } from "@playwright/test";
 import { BasePage } from "./BasePage";
-
+import { LoginSelectors } from "../../utils/selectors";
 export class LoginPage extends BasePage {
   readonly username: Locator;
   readonly password: Locator;
@@ -9,10 +9,10 @@ export class LoginPage extends BasePage {
 
   constructor(page: Page) {
     super(page);
-    this.username = page.locator('[data-test="username"]');
-    this.password = page.locator('[data-test="password"]');
-    this.buttonLogin = page.locator('[data-test="login-button"]');
-    this.errorMessage = page.locator('[data-test="error"]');
+    this.username = page.locator(LoginSelectors.usernameInput);
+    this.password = page.locator(LoginSelectors.passwordInput);
+    this.buttonLogin = page.locator(LoginSelectors.loginButton);
+    this.errorMessage = page.locator(LoginSelectors.errorMessage);
   }
 
   async gotoLoginPage() {
---
+++ utils/selectors.ts
@@ -0,0 +1,11 @@
+export const LoginSelectors = {
+  usernameInput: '[data-test="username"]',
+  passwordInput: '[data-test="password"]',
+  loginButton: '[data-test="login-button"]',
+  errorMessage: '[data-test="error"]',
+};
+
+export const ProductSelectors = {
+  addToCartSauceLabsBikeLight: '[data-test="add-to-cart-sauce-labs-bike-light"]',
+};
+
